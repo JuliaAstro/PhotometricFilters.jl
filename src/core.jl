7cca4ab0fcc9381a0e61021a4e1811ca618777a3
Abhro R <5664668+abhro@users.noreply.github.com>
2025-03-18 22:26:55 -0400

    Remove link to non-existent method

--- src/core.jl
+++ src/core.jl
@@ -123,9 +123,6 @@ end
     apply(::PhotometricFilter, wave, flux)
 
 Use linear interpolation to map the wavelenghts of the photometric filter to the given wavelengths and apply the filter throughput to the `flux`. The wavelengths of the filter and `wave` need to be compatible. This means if one has units, the other one needs units, too.
-
-# See also
-[`interpolator`](@ref)
 """
 apply(filt::PhotometricFilter, wave, flux) = apply!(filt, wave, flux, similar(flux))
 
