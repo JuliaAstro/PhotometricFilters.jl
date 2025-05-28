4ab933285dfb2438afab4c0d0d01a37a15b721cc
cgarling <chris.t.garling@gmail.com>
2025-05-28 13:18:12 -0400

    Add docstring and doctest to `get_filter`

--- src/svo.jl
+++ src/svo.jl
@@ -4,8 +4,30 @@ import VOTables
 const svo_url = "http://svo2.cab.inta-csic.es/theory/fps/fps.php"
 const detector_type = (Energy(), Photon()) # SVO returns 0 or 1 for energy or photon
 
-function get_filter(fname::AbstractString)
-    response = HTTP.get(svo_url; query = Dict("ID" => fname))
+"""
+    get_filter(filtername::AbstractString)
+
+Query the online [SVO filter service](http://svo2.cab.inta-csic.es/theory/fps) for the photometric filter `filtername`, which must adhere to their naming scheme. Returns an instance of [`PhotometricFilter`](@ref PhotometricFilters.PhotometricFilter). 
+
+# Examples
+```jldoctest
+julia> using PhotometricFilters: get_filter
+
+julia> get_filter("2MASS/2MASS.J")
+107-element PhotometricFilter{Float64}: 2MASS/2MASS.J
+ min. wave.: 10820.0 Å
+ max. wave.: 14060.0 Å
+ effective wave.: 12285.654731403807 Å
+ central wave.: 12410.5170694321 Å
+ pivot wave.: 12358.089456559974 Å
+ eff. width: 1624.3245065600008 Å
+ fwhm: 2170.0 Å
+```
+"""
+function get_filter(filtername::AbstractString)
+    # PhotometricFilter constructor requires String, not AbstractString, so convert
+    filtername = String(filtername)
+    response = HTTP.get(svo_url; query = Dict("ID" => filtername))
     if response.status != 200 # If status is not normal,
         @info "HTTP request to SVO returned with status code $(response.status)."
     end
@@ -18,5 +40,5 @@ function get_filter(fname::AbstractString)
     return PhotometricFilter(table.Wavelength,
                              Vector(table.Transmission);
                              detector=detector_type[dtype],
-                             name=fname)
+                             name=filtername)
 end
