86201dc2327e92f4fb6532fde80c6ff29a5a8f60
cgarling <chris.t.garling@gmail.com>
2025-06-03 15:50:39 -0400

    Start new docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,23 @@
 using PhotometricFilters
-using Documenter
+using Documenter: makedocs, HTML, deploydocs
 using Documenter.Remotes: GitHub
 
 makedocs(;
-    modules=[PhotometricFilters],
-    authors="Miles Lucas <mdlucas@hawaii.edu> and contributors",
-    repo=GitHub("JuliaAstro/PhotometricFilters.jl"),
-    sitename="PhotometricFilters.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://juliaastro.github.io/PhotometricFilters.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
-)
+         sitename = "PhotometricFilters.jl",
+         modules = [PhotometricFilters],
+         authors = "Miles Lucas <mdlucas@hawaii.edu>, Chris Garling, and contributors",
+         repo = GitHub("JuliaAstro/PhotometricFilters.jl"),
+         format = HTML(;
+                       prettyurls = get(ENV, "CI", nothing) == "true",
+                       canonical = "https://juliaastro.org/PhotometricFilters/stable/",
+                       assets = String[],
+                       ),
+         pages = [
+                  "Home" => "index.md",
+                 ],
+         )
 
 deploydocs(;
-    repo="github.com/JuliaAstro/PhotometricFilters.jl",
-)
+           repo = "github.com/JuliaAstro/PhotometricFilters.jl",
+           versions = ["stable" => "v^", "v#.#"] # Restrict to minor releases
+           )
