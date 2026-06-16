e67557d2ed04c6be85f1f0d2ce0df131980557f6
abhro <5664668+abhro@users.noreply.github.com>
2026-06-15 22:02:56 -0400

    Put doctest in larger testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Test, SafeTestsets
 # Run doctests
 using Documenter: DocMeta, doctest
 DocMeta.setdocmeta!(PhotometricFilters, :DocTestSetup, :(using PhotometricFilters; using Logging; disable_logging(Logging.Info)); recursive=true)
-doctest(PhotometricFilters)
 
 @testset verbose=true "PhotometricFilters.jl" begin
+    doctest(PhotometricFilters)
     @safetestset "Library" include("library_tests.jl")
     @safetestset "Iteration" include("iteration_tests.jl")
     @safetestset "SVO tests" include("svo_tests.jl")
